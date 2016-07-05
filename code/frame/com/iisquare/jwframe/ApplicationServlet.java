29a3d985b2706338b8778eeb17bb5c4948494acf
iisquare <iisquare@163.com>
2016-07-05 10:59:01 +0800

    ApplicationServlet

--- code/frame/com/iisquare/jwframe/ApplicationServlet.java
+++ code/frame/com/iisquare/jwframe/ApplicationServlet.java
@@ -1,17 +1,29 @@
 package com.iisquare.jwframe;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 public class ApplicationServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	private WebApplicationContext wac;
+	private String appUri, rootPath;
 
 	@Override
 	public void init() throws ServletException {
+		wac = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+		appUri = getInitParameter("appUri");
+		if (!appUri.startsWith("/")) appUri = "/" + appUri;
+		if (!appUri.endsWith("/")) appUri += "/";
+		rootPath = getServletContext().getRealPath("/");
+		System.out.println(appUri + "\r\n" + rootPath);
 		super.init();
 	}
 
