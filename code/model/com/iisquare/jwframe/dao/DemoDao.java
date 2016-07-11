2942e48f76871ea0c891ca53c0dcae8b56975e97
iisquare <iisquare@163.com>
2016-07-11 15:45:07 +0800

    DemoDao columns

--- code/model/com/iisquare/jwframe/dao/DemoDao.java
+++ code/model/com/iisquare/jwframe/dao/DemoDao.java
@@ -1,5 +1,8 @@
 package com.iisquare.jwframe.dao;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +15,16 @@ public class DemoDao extends MySQLBase {
 	@Override
 	public String tableName() {
 		return tablePrefix() + "demo";
+	}
+
+	@Override
+	public LinkedHashMap<String, Map<String, Object>> columns() {
+		LinkedHashMap<String, Map<String, Object>> columns = new LinkedHashMap<>();
+		columns.put("id", null); // 主键
+		columns.put("parent_id", null); // 父级
+		columns.put("name", null); // 名称
+		columns.put("status", null); // 状态
+		return columns;
 	}
 
 }
